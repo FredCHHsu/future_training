297a1e7e4e824c49857c8e6e8c07da5417f7aec1
Fred <fred@barbiedeiMac-2.local>
2016-04-26 13:06:50 +0800

    Add Airbnb Eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,47 @@
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/index.js',
   ],
   output: {
-    path: __dirname + "/public/",
+    path: __dirname + '/public/',
     publicPath: '/public/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        configFile: './.eslintrc',
+        exclude: /node_modules/,
+      },
+    ],
     loaders: [
       {
-        exclude: /node_modules/,
+        test: /\.js$/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015']
-        }
+          presets: ['react', 'es2015'],
+        },
+        exclude: /node_modules/,
       },
       {
-        exclude: /node_modules/,
         test: /\.scss$/,
-        loaders: ["style", "css", "resolve-url", "sass?sourceMap"]
-      }
-    ]
+        loaders: ['style', 'css', 'resolve-url', 'sass?sourceMap'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  eslint: {
+    failOnError: false,
+    failOnWarning: false,
   },
   resolve: {
-    extensions: ['', '.js', '.jsx' ,'.scss']
+    extensions: ['', '.js', '.jsx', '.scss'],
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
-  }
+    contentBase: './',
+  },
 };
