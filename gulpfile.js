c5012eb2dc8743d9d74c00d8a82c7bf25a3b4c9c
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2016-05-22 14:50:55 +0800

    Rebuild PostCSS Processors

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,57 @@
-// require
+// require gulp and sass core
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
 
 // PostCSS
 const postcss = require('gulp-postcss');
-const reporter = require('postcss-reporter');
+// Pre-SASS processors
 const scssSyntax = require('postcss-scss');
-const autoprefixer = require('autoprefixer');
 const stylelint = require('stylelint');
 const doiuse = require('doiuse');
-const processors = [
-  stylelint(),
-  doiuse({ browsers: ['ie >= 11', 'last 2 version'] }),
-  autoprefixer({ browsers: ['> 5%', 'last 2 version'] }),
-  reporter(),
-];
+const reporter = require('postcss-reporter');
+// After-SASS processors
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 // require and init browserSync
 const browserSync = require('browser-sync').create();
 
-// const
+// URL const
 const devUrl = 'http://localhost:8080/'; // equal to webpack entry
-const src = './style/style.scss';
+const entryFile = './style/style.scss';
+const src = './style/**/*.scss';
 const dest = './public';
 
-gulp.task('sass', () =>
+const preSASSProcessors = [
+  stylelint(),
+  doiuse({ browsers: ['ie >= 11', 'last 2 version'] }),
+  reporter({ clearMessages: true }),
+];
+
+const afterSASSProcessors = [
+  autoprefixer({ browsers: ['> 5%', 'last 2 version'] }),
+];
+
+const afterProcessorsProduction = [
+  autoprefixer({ browsers: ['> 5%', 'last 2 version'] }),
+  cssnano(),
+];
+
+// PostCSS before Sass compilation
+gulp.task('pre-sass', () =>
   gulp.src(src)
-  .pipe(postcss(processors, { syntax: scssSyntax }))
+  .pipe(postcss(preSASSProcessors, { syntax: scssSyntax }))
+);
+
+// Sass compilation with PostCSS
+gulp.task('sass', () =>
+  gulp.src(entryFile)
   .pipe(sourcemaps.init())
   .pipe(sass({
     includePaths: ['./node_modules/susy/sass/'],
   }).on('error', sass.logError))
+  .pipe(postcss(afterSASSProcessors))
   .pipe(sourcemaps.write('./')) // relative to the dest path for seperated map file
   .pipe(gulp.dest(dest))
   .pipe(browserSync.stream())
@@ -45,17 +64,16 @@ gulp.task('browserSync', () =>
 );
 
 gulp.task('watch', ['browserSync'], () =>
-  gulp.watch('./style/**/*.scss', ['sass'])
+  gulp.watch('./style/**/*.scss', ['pre-sass', 'sass'])
 );
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['pre-sass', 'sass', 'watch']);
 
 gulp.task('production', () =>
-  gulp.src(src)
-  .pipe(postcss(processors, { syntax: scssSyntax }))
+  gulp.src(entryFile)
   .pipe(sass({
     includePaths: ['./node_modules/susy/sass/'],
   }).on('error', sass.logError))
-  .pipe(cssnano())
+  .pipe(postcss(afterProcessorsProduction))
   .pipe(gulp.dest(dest))
 );
